f6c2185536639f8c1ee8158abe689df5272c87f0
Rosenfeld99 <124917070+Rosenfeld99@users.noreply.github.com>
2024-06-16 16:26:37 +0300

    add utils components

--- src/test/Test.jsx
+++ src/test/Test.jsx
@@ -1,26 +1,15 @@
+import ButtonAction from "../utils/ButtonAction";
+import Navbar from "../utils/Navbar";
 import BACKPAPER from "/backPaper.png"
 import CHECKMARK from "/CheckMark.png"
-import LEFTMENU from "/Left.png"
-import MENUHUM from "/Menu.png"
+
 
 export default function Test() {
 
     return (
         <div className="flex flex-col pb-20 mx-auto w-full bg-white min-h-screen flex-1 max-w-[480px] rounded-[35px] ">
 
-            <div className="flex gap-5 items-center px-2.5 py-2 w-full text-2xl font-semibold tracking-tight leading-9 text-center text-white bg-gradient-to-r from-cyan-500 to-blue-500">
-                <img
-                    loading="lazy"
-                    srcSet={LEFTMENU}
-                    className="shrink-0 w-10 aspect-square"
-                />
-                <div className="flex-auto self-stretch my-auto">App name</div>
-                <img
-                    loading="lazy"
-                    srcSet={MENUHUM}
-                    className="shrink-0 w-10 aspect-square"
-                />
-            </div>
+            <Navbar />
             <div className="flex gap-5 self-center px-5 mt-8 text-xs leading-5 text-center text-black">
                 <div className="grow my-auto">
                     דיווח אחרון היום הייתם ב{" "}
@@ -64,9 +53,7 @@ export default function Test() {
                         תינתן אפשרות של סיום דיווח
                     </div>
                     {/*  */}
-                    <div className="justify-center items-center self-stretch px-4 py-1.5 text-xl font-medium leading-7 text-center text-white rounded-lg bg-stone-700 max-w-[327px]">
-                        שלח דיווח
-                    </div>
+                    <ButtonAction title="שלח דיווח" />
                 </div>
             </div>
         </div>
