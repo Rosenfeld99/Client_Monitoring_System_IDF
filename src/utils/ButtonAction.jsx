9da681f58005a62a075951ea72aca487fa27770f
Rosenfeld99 <124917070+Rosenfeld99@users.noreply.github.com>
2024-06-18 03:30:55 +0300

    add animation for navigation and history page

--- src/utils/ButtonAction.jsx
+++ src/utils/ButtonAction.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ButtonAction = ({title}) => {
     return (
-        <div className="justify-center items-center self-stretch px-4 py-1.5 text-xl font-medium leading-7 text-center text-white rounded-lg bg-stone-700 max-w-[327px]">
+        <div className="justify-center items-center self-stretch px-4 py-1.5 text-xl font-medium leading-7 text-center text-white rounded-lg bg-stone-700 max-w-[427px]">
             {title}
         </div>)
 }
