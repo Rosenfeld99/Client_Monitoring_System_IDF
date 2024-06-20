c6a6fa9fcdcfc972f9563d817481498fcb3125a7
Nehoray <nehorayatia1@gmail.com>
2024-06-20 09:31:14 +0300

    search result finish

--- src/components/admin/advanceSearch/SearchResult.jsx
+++ src/components/admin/advanceSearch/SearchResult.jsx
@@ -13,34 +13,30 @@ function SearchResult() {
                 <Navbar />
             </div>
             <div className='min-h-[85vh]  flex-col flex justify-center items-center'>
-                <div className='w-full flex flex-col items-center justify-center'>
+                <div className='w-full flex mr-1  flex-col items-center justify-center'>
                     <h1 className='font-bold text-xl'>תוצאות חיפוש מתקדם</h1>
                     <p>
                         לפי
                     </p>
-                    <div className='ml-auto'>
-                        <div className='items-center flex'>
-
+                    <div className='w-[90vw] items-end flex flex-col'>
+                        <div className='mb-1 font-bold'>
+                            <span dir='rtl' className='font-normal mr-1'>יחדחגח</span>
                             :תאריך
-                            <BsCalendarDate className='ml-2' />
-
+                            <BsCalendarDate className='ml-2 inline-block' />
                         </div>
-                        <div className='items-center flex'>
-
+                        <div className='mb-1 font-bold'>
+                            <span dir='rtl' className='font-normal mr-1'>ידגבחדג</span>
                             :מקומות
-                            <LiaPlaceOfWorshipSolid />
-
+                            <LiaPlaceOfWorshipSolid className=' inline-block' />
                         </div>
-                        <div className='items-center flex'>
-
-
+                        <div className='mb-1 font-bold'>
+                            <span dir='rtl' className='font-normal mr-1'>יחח</span>
                             :אנשים
-                            <MdOutlinePeopleAlt className='ml-2' />
+                            <MdOutlinePeopleAlt className='ml-2 inline-block' />
                         </div>
                     </div>
                 </div>
-                <div className='mx-1'>
-
+                <div className='m-4 w-[90vw] px-3 border '>
                     <UsersDisplay />
                 </div>
 
--- src/components/admin/solidersReports/UsersDisplay.jsx
+++ src/components/admin/solidersReports/UsersDisplay.jsx
@@ -5,10 +5,10 @@ const DisplayUser = () => {
     const [chooseReport, setChooseReport] = useState(false)
     return (<>
         <div onClick={() => setChooseReport(!chooseReport)} className={` ${chooseReport && 'bg-[#e9f6fd] border-[1px]  border-[#62bcee] rounded-lg'} min-h-10 grid grid-cols-12 my-1 gap-3 py-1 `} >
-            <div className="flex w-full col-span-2 items-center justify-center ">עושה</div>
-            <div className="flex w-full  col-span-3 items-center justify-center ">חדר אוכל</div>
-            <div className="flex w-full col-span-4 items-center justify-center ">דיווח אחרון</div>
-            <div className="flex w-full col-span-3 items-center  justify-center ">משתמש</div>
+            <div className="flex w-full h-full overflow-y-auto col-span-2 items-center justify-center ">עושה</div>
+            <div className="flex w-full h-full overflow-y-auto  col-span-3 items-center justify-center ">חדר  hh jjj nn jjj אוכל</div>
+            <div className="flex w-full h-full overflow-y-auto col-span-4 items-center justify-center ">דיווח אחרון</div>
+            <div className="flex w-full h-full overflow-y-auto col-span-3 items-center  justify-center ">משתמש</div>
         </div>
         <div className='divide-solid divide-y border  ' ></div>
     </>)
@@ -26,7 +26,7 @@ function UsersDisplay() {
             <div className="flex w-full col-span-3 items-center  justify-center font-semibold">משתמש</div>
         </div>
             <div className='divide-solid divide-y border mt-1 ' ></div>
-            <div className='  max-h-64 overflow-y-auto'>
+            <div className='  max-h-72 overflow-y-auto'>
                 {new Array(15).fill(0).map((_, i) =>
                     <div key={i}>
                         <DisplayUser key={i} />
