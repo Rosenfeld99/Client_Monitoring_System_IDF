c05d1403a7b3ab589344534fe3629f730d5f7066
Rosenfeld99 <124917070+Rosenfeld99@users.noreply.github.com>
2024-07-08 19:23:00 +0300

    Craete starct and design fpr charts view admin

--- src/utils/Charts/ChartPei.jsx
+++ src/utils/Charts/ChartPei.jsx
@@ -2,17 +2,17 @@
 import React from 'react';
 import CanvasJSReact from '@canvasjs/react-charts';
 //var CanvasJSReact = require('@canvasjs/react-charts');
- 
+
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-const ChartPei = () => {
+const ChartPei = ({ dataPoints }) => {
 	const options = {
 		// exportEnabled: true,
 		animationEnabled: true,
-        // theme: "dark1", // "light1", "dark1", "dark2"
-		title: {
-			text: "ניתוח נתונים"
-		},
+		// theme: "dark1", // "light1", "dark1", "dark2"
+		// title: {
+		// 	text: "ניתוח נתונים"
+		// },
 		data: [{
 			type: "pie",
 			startAngle: 75,
@@ -21,19 +21,14 @@ const ChartPei = () => {
 			legendText: "{label}",
 			indexLabelFontSize: 16,
 			indexLabel: "{label} - {y}%",
-			dataPoints: [
-				{ y: 25, label: "מחוץ לבסיס" },
-				{ y: 70, label: "בסיס" },
-				{ y: 5, label: "שטח" },
-			
-			],
+			dataPoints
 		}]
 	};
 
 	return (
 		<div className=' relative z-20'>
 			<CanvasJSChart options={options} />
-            <div className=" absolute bg-light_primary dark:bg-dark_primary bottom-0 right-0 w-16 h-3"/>
+			<div className=" absolute bg-light_primary dark:bg-dark_primary bottom-0 right-0 w-16 h-3" />
 		</div>
 	);
 };
